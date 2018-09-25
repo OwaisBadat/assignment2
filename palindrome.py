db3d2d91e4881d8a0b6aaa1ea59be1e09fb37f3b
Owais Badat <owaisbadat@gmail.com>
2018-09-24 23:06:29 -0500

    new manual palindrome function

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,39 @@
-user_input = str(input("What is the your word: "))
+# user_input = str(input("What is the your word: "))
 
 
 #extened slice syntax
 #slice[start:stop:step]
 #colon instead of an index number will assume "start" for first color "end" for second colon
 #if step is negative it will run in reverse
-reverse_word = user_input[::-1])
+# reverse_word = user_input[::-1])
+#
+# def is_palindrome(reverse_word):
+#     if(reverse_word == user_input):
+#         return("{0} is a palindrome!".format(user_input))
+#     else:
+#         return("{0} is NOT a palindrome!".format(user_input))
+# print(is_palindrome(reverse_word))
 
-def is_palindrome(reverse_word):
-    if(reverse_word == user_input):
-        return("{0} is a palindrome!".format(user_input))
+#or
+
+user_input2 = str(input("What is the your word: "))
+
+
+def backwards_user_input(user_input2):
+    #defining the variable to store the concatenated letters
+    reversed_word = ""
+    for index in range(len(user_input2) - 1, -1, -1):
+        #adding the letters to the variable beginning from the last index
+        reversed_word = reversed_word + user_input2[index]
+    return reversed_word
+
+backwards = backwards_user_input(user_input2)
+
+
+def palindrome(user_input2):
+    if (user_input2 == backwards):
+        print('palindrome')
     else:
-        return("{0} is NOT a palindrome!".format(user_input))
-print(is_palindrome(reverse_word))
+        print("not a palindrome")
+
+palindrome(user_input2)
---
+++ tempconverter.py
@@ -0,0 +1,5 @@
+user_input = int(input("What is the temperature in Celsius: "))
+
+f = int(user_input * (9/5)) + 32
+
+print("{0} degrees celsius is equal to {1} degrees fahrenheit".format(user_input,f))
